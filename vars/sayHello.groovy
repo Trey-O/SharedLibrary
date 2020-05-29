1ce3f7a57f240a9862eec497e6a92ce710d88625
Trey-O <o.trey@ymail.com>
2020-05-29 16:03:21 -0400

    second checkout

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,5 +2,6 @@
 
 def call(String name = 'human') {
   checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], gitTool: 'Default', submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Trey-O/pipeline-deployment-demo']]])
+  checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], gitTool: 'Default', submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Trey-O/mvntest-core']]])
   echo "Hello, ${name}."
 }
