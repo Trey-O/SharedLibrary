6c72162eb7e8927de3a149b6f4075d721c3a810b
Trey-O <o.trey@ymail.com>
2020-05-29 13:52:48 -0400

    Update-SharedLibrary-File to checkout Jenkinsfile

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String name = 'human') {
+  checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], gitTool: 'Default', submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Trey-O/pipeline-deployment-demo']]])
   echo "Hello, ${name}."
 }
