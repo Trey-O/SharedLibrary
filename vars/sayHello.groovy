6018d795961a23065b798025fe25f28b3d408b80
Trey-O <o.trey@ymail.com>
2020-05-29 16:30:29 -0400

    Update SHared Library

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -4,4 +4,3 @@ def call(String name = 'human') {
   //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], gitTool: 'Default', submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Trey-O/pipeline-deployment-demo']]])
   checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], gitTool: 'Default', submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Trey-O/mvntest-core']]])
   echo "Hello, ${name}."
-}[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], gitTool: 'Default', submoduleCfg: [], u
