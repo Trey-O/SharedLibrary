1eb848364be86746257ba1c486de114330122db4
Trey-O <o.trey@ymail.com>
2020-06-04 09:45:24 -0400

    Edit setup sharedlibrary

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,8 +1,11 @@
 #!/usr/bin/env groovy
 
 def call(String name = 'human') {
-  checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], gitTool: 'Default', submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Trey-O/pipeline-deployment-demo']]])
-  checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], gitTool: 'Default', submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Trey-O/mvntest-core']]])
-  echo "Hello, ${name}."
+  checkout([$class: 'GitSCM', branches: [[name: '*/master']], 
+            doGenerateSubmoduleConfigurations: false, extensions: [], 
+            gitTool: 'Default', submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Trey-O/pipeline-deployment-demo']]])
+  checkout([$class: 'GitSCM', branches: [[name: '*/master']], 
+            doGenerateSubmoduleConfigurations: false, extensions: [], 
+            gitTool: 'Default', submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Trey-O/mvntest-core']]])
 
 }
